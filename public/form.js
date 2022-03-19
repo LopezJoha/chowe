f820dbebb43c1ec859c56af0eef15b991564e0fa
Jeidy Johana Lopez <jeidylopez@outlook.es>
2022-03-18 20:48:37 -0500

    Caldiiiito

---
+++ public/form.js
@@ -0,0 +1,62 @@
+function sendEmail() {
+    let nombreForm = document.getElementById("nombre");
+    let celForm = document.getElementById("celular");
+    let correoForm = document.getElementById("correo");
+    let mensajeForm = document.getElementById("mensaje");
+      
+
+	Email.send({
+        Host: "smtp.gmail.com",
+        Username : "chowebogota@gmail.com",
+        Password : "Admin1234567890*",
+        To : `${correoForm.value}, chowebogota@gmail.com`,
+        From : "chowebogota@gmail.com",
+        Subject : "Confirmación Correo Electrónico",
+        Body : `Este mensaje fue remitido por:${correoForm.value}, los datos de contacto proporcionados son
+            Nombre: ${nombreForm.value} y el mensaje es el siguiente: ${mensajeForm.value}` 
+	}).then(function (message){
+         alert("Mensaje Enviado Exitosamente");
+         document.getElementById("formulario").reset();
+    });
+};
+
+
+function moveMenu(){
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header_Lista2").className = "cambio_Lista2";
+        document.getElementById("header_Lista3").className = "cambio_Lista3";
+        document.getElementById("header_Cont2").className = "cambio_header_Cont2";
+        document.getElementById("arrowUp").style.visibility="visible";
+        document.getElementById("borde-Img").className="borde-img";
+        document.getElementById("imagen-Header").className="img-header";
+    }else {
+        document.getElementById("header_Lista2").className = "header-contenido1__list2";
+        document.getElementById("header_Lista3").className = "header-contenido2__list1";
+        document.getElementById("header_Cont2").className = "header-contenido2";
+        document.getElementById("arrowUp").style.visibility="hidden"; 
+        document.getElementById("borde-Img").className="Img-Borde";
+        document.getElementById("imagen-Header").className="header-imagen";
+
+      }
+};
+
+
+
+
+
+
+
+
+function isPrime(input) {
+    if (input === 0 || input ===1 || input ===4){
+        return false;
+        
+    }else{ for (let x = 2; x < input; x++){
+        if (input % x == 0){
+            return false
+        }
+    }return true
+        
+    }
+
+}
--- public/menu.js
+++ public/menu.js
@@ -7,23 +7,23 @@ function getMenuItems(opcionMenu){
     };
     
     if (opcionMenu !== "salsas"){
-        let lista = "<ul style=\"display: flex; flex-wrap: wrap; justify-content: center;\">";
-        
-        
+        let lista = "<ul class=\"food-List\">";
+                
         for(let i = 0; i < menuItems[opcionMenu].length; i++){
             var obj= menuItems[opcionMenu][i];
             
             lista+= 
-            `<li style="display: flex; width: 40%; height: 10rem; position: relative; padding-left: 110px;">
-                <div style="position: absolute; top: 50%; left: 0; transform: translateY(-50%);"> 
+            `<li class="food-List_element">
+                <div class="List-element" > 
                     <img src=${obj.imagen} height="100px" width="100px" style="border-radius: 50px;"/>
                 </div>
-                <div style="width: 75%; margin-left: 20px;">
-                    <div style="display: flex; border-bottom: 2px dotted black; height: 50%; justify-content: space-between; align-items: center;">
-                        <h1>${obj.title}</h1>
-                        <p>$${obj.price}</p>
+
+                <div class="List-element-one">
+                    <div class="List-sub-element-one">
+                        <h1 class="Subtitulo">${obj.title}</h1>
+                        <p class="parrafo-menu">$${obj.price}</p>
                     </div>
-                    <p style="height: 50%; text-align: justify; top: 0; margin: 2.5px;">${obj.par}</p>
+                    <p class="parrafo-menu List-element-two">${obj.par}</p>
                 </div>
             </li>`
         }
@@ -40,78 +40,82 @@ function getMenuItems(opcionMenu){
             var obj= menuItems[opcionMenu][i];
             
             lista+=
-            `<li style="display: flex; flex-direction: column;">
-                <p style=" text-align: justify; margin: 2.5px; font-size: 2rem; justify-content: center; align-items: center; padding: 1rem;">${obj.nombre}</p>
+            `<li class ="food-List_else">
+                <p class="parrafo_else>${obj.nombre}</p>
             </li>`
         }
 
         lista +="</ul>" 
 
         var contenido = document.getElementById("allTheFood");
-        contenido.innerHTML=lista;
-        
+        contenido.innerHTML=lista        
     }
-        
 }
 
 
 var menuItems = {
     entradas:[
         {
-            imagen:"/public/Imagen/aros.jpg",
-            title:"Aros de cebolla",
-            par:"Deliciosos aritos de cebolla, crujientes acompañados con la salsa de tu preferencia",
-            price:4000
+        imagen:"/public/Imagen/aros.jpg",
+        title:"Aros de cebolla",
+        par:"Deliciosos aritos de cebolla crujientes.",
+        price:4000
         },
         {
-            imagen:"/public/Imagen/chicharron.jpg",
-            title:"Chicharrones",
-            par:"Trocitos de chicharron, acompañados piezas de maiz con la salsa de tu preferencia ",
-            price:4000
+        imagen:"/public/Imagen/chicharron.jpg",
+        title:"Chicharrones",
+        par:"Trocitos de chicharron, acompañados piezas de maiz.",
+        price:4000
         },
         {
-            imagen:"/public/Imagen/papas1.jpg",
-            title:"Papas a la Francesa",
-            par:"Exquisitos bastoncitos de papas freidos,acompañados con la salsa de tu preferencia ",
-            price:4000
+        imagen:"/public/Imagen/papas1.jpg",
+        title:"Papas a la Francesa",
+        par:"Exquisitos bastoncitos de papas freidos.",
+        price:4000
         },
         {
-            imagen:"/public/Imagen/papas2.jpg",
-            title:"Papas Artesanales",
-            par:"Deliciosas papas en casco, acompañados con la salsa de tu preferencia",
-            price:4000
+        imagen:"/public/Imagen/papas2.jpg",
+        title:"Papas Artesanales",
+        par:"Deliciosas papas en cascos.",
+        price:4000
         },
         {
-            imagen:"/public/Imagen/yuquitas.jpg",
-            title:"Croquetas de Yuca",
-            par:"Deliciosas tiras de Yuca freidas, acompañados con la salsa de tu preferencia",
-            price:4000
+        imagen:"/public/Imagen/yuquitas.jpg",
+        title:"Croquetas de Yuca",
+        par:"Deliciosas tiras de Yuca freidas.",
+        price:4000
+        },
+        {
+        imagen:"/public/Imagen/yuquitas.jpg",
+        title:"Croquetas de Yuca",
+        par:"Deliciosas tiras de Yuca freidas.",
+        price:4000
         },
     ],
     combos:[{
         imagen:"/public/Imagen/opc1.jpg",
-        title:`<span id="numeroAlitas">8</span>Piezas de Alitas`,
+        title:`<span id="numeroAlitas">8</span> Piezas de alitas`,
         par:"Bañadas en 1 salsa",
         price: 15900
         },
         {
-            imagen:"/public/Imagen/opc2.jpg",
-            title:`<span id="numeroAlitas">16</span>Piezas de Alitas`,
-            par:"Bañadas en 2 salsas",
-            price: 27900
+        imagen:"/public/Imagen/opc2.jpg",
+        title:`<span id="numeroAlitas">16</span> Piezas de alitas`,
+        par:"Bañadas en 2 salsas",
+        price: 27900
             },
         {
-            imagen:"/public/Imagen/opc3.jpg",
-            title:`<span id="numeroAlitas">24</span>Piezas de Alitas`,
-            par:"Bañadas en 3 salsas",
-            price: 37900
-            },
+        imagen:"/public/Imagen/opc3.jpg",
+        title:`<span id="numeroAlitas">24</span> Piezas de alitas`,
+        par:"Bañadas en 3 salsas",
+        price: 37900
+        },
         {
-            imagen:"/public/Imagen/opc5.jpg",
-            title:`<span id="numeroAlitas">32</span>Piezas de Alitas`,
-            par:"Bañadas en 4 salsas",
-            price: 47900
-            },        
+        imagen:"/public/Imagen/opc5.jpg",
+        title:`<span id="numeroAlitas">32</span> Piezas de alitas`,
+        par:"Bañadas en 4 salsas",
+        price: 47900
+        },        
     ],
     salsas:[
         {nombre: "Bbq Chowé"}, 
@@ -121,24 +125,32 @@ var menuItems = {
         {nombre: "Teriyaki"}, 
         {nombre: "Búfalo Picante"}, 
         {nombre: "Especial Chowé"}],
-    bebidas:[{
-            imagen:"/public/Imagen/soda.jpg",
-            title:"Gaseosa 400 ml",
-            par:"Productos marca Postobón",
-            price:2500
-            },
-            {
-            imagen:"/public/Imagen/soda.jpg",
-            title:"Gaseosa 1.5 L",
-            par:"Productos marca Postobón",
-            price:4000
-            },
-            {
-            imagen:"/public/Imagen/soda.jpg",
-            title:"Gaseosa 3.125 L",
-            par:"Productos marca Postobón",
-            price:6000
-            },]
+    bebidas:[
+        {
+        imagen:"/public/Imagen/soda.jpg",
+        title:"Gaseosa 400 ml",
+        par:"Productos marca Postobón",
+        price:2500
+        },
+        {
+        imagen:"/public/Imagen/soda.jpg",
+        title:"Gaseosa 1.5 L",
+        par:"Productos marca Postobón",
+        price:4000
+        },
+        {
+        imagen:"/public/Imagen/soda.jpg",
+        title:"Gaseosa 3.125 L",
+        par:"Productos marca Postobón",
+        price:6000
+        },
+        {
+        imagen:"/public/Imagen/soda.jpg",
+        title:"Gaseosa 3.125 L",
+        par:"Productos marca Postobón",
+        price:6000
+        },
+        ]
 };
 
 //LLAMADA A LA FUNCION PARA MOSTRAR CONTENIDO
